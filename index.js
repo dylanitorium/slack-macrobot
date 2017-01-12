f478fcd9a3718d77902b7913f1c7ea9589f9ad7d
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-12 21:08:13 +1300

    fixing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const bl = require('bl');
 const options = {
   host: 'www.neogaf.com',
   path: '/forum/showthread.php?t='
@@ -10,15 +11,16 @@ const app = express();
 app.use('/gaf', (req, res) => {
   const id = req.params.text.trim();
   options.path += id;
-  http.get(options, (getRes){
-  //
+  http.get(options, (getRes) => {
+    console.log(getRes);
+    getRes.pipe(bl((err, data) => {
+      res.json({
+        "response_type": "in_channel",
+        "text": id
+      });
+    }));
   });
-  res.json({
-    "response_type": "in_channel",
-    "text": id
 });
-});
-
 app.listen(8889, () => {
   console.log('listening');
 });
