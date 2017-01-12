e2d1bb2a390b82e39d29d4fd96b97d0fa0266e5a
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-13 08:46:22 +1300

    adding carbs

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ const app = express();
 app.use('/macros', (req, res) => {
   const item = req.query.text.trim();
 
+  const fields = [
+
+  ];
+
   var path = '/v1_1/search/';
   path += encodeURI(item);
-  path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_protein';
+  path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_protein%2Cnf_total_carbohydrate';
   path += '&appId=38256e6d&appKey=66431bb4bbec110e138f0a677cad31f3';
 
   http.get({
@@ -27,9 +31,13 @@ app.use('/macros', (req, res) => {
     })
     getRes.pipe(concat((buf) => {
       var data = JSON.parse(buf.toString()).hits[0].fields;
+      var message = `${data.item_name}(${data.brand_name})`;
+      message += ` - ${data.nf_calories}cal / ${data.nf_protein}g (protein) /`
+      message += ` ${data.nf_total_carbohydrates}g (carbs) / ${data.nf_total_fat}g (fat)`;
+
       res.json({
         "response_type": "in_channel",
-        "text": `${data.item_name}(${data.brand_name}) - ${data.nf_calories}cal / ${data.nf_protein}g (protein) / ${data.nf_total_fat}g (total fat)`
+        "text": message,
       });
     }));
   });
