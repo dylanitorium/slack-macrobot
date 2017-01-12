944dfaba37cd0180ecc2134dd13a03c2e8deaa0f
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-12 22:56:27 +1300

    adding macros

--- index.js
+++ index.js
@@ -10,20 +10,45 @@ const options = {
 
 const app = express();
 
-app.use('/gaf', (req, res) => {
-  const id = req.query.text.trim();
-  http.get({
-    host: 'www.neogaf.com',
-    path: `/forum/showthread.php?t=${id}`,
+// app.use('/gaf', (req, res) => {
+//   const id = req.query.text.trim();
+//   http.get({
+//     host: 'www.neogaf.com',
+//     path: `/forum/showthread.php?t=${id}`,
+//   }, (getRes) => {
+//     getRes.on('error', (err) => {
+//       console.log(error);
+//     })
+//     getRes.pipe(concat((data) => {
+//       const $ = cheerio.load(data.toString())
+//       res.json({
+//         "response_type": "in_channel",
+//         "text": $('title').text(),
+//       });
+//     }));
+//   });
+// });
+app.use('/macros', (req, res) => {
+  const item = req.query.text.trim();
+
+  var path = 'v1_1/search/';
+  path += encodeURI(item);
+  path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat';
+  path += '&appId=38256e6d&appKey=66431bb4bbec110e138f0a677cad31f3';
+
+  https.get({
+    host: 'api.nutritionix.com/',
+    path: path,
   }, (getRes) => {
     getRes.on('error', (err) => {
       console.log(error);
     })
     getRes.pipe(concat((data) => {
-      const $ = cheerio.load(data.toString())
+      var data = data.toString();
+      data = data.hits[0].fields;
       res.json({
         "response_type": "in_channel",
-        "text": $('title').text(),
+        "text": `${data.item_name}(${data.brand_name}) ${data.nf_calories}(cal) ${data.nf_total_fat}g(total fat)`
       });
     }));
   });
