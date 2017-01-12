feca5d11e04c2eb15b2dfad0fb600e8db7595262
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-13 08:50:13 +1300

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use('/macros', (req, res) => {
       var data = JSON.parse(buf.toString()).hits[0].fields;
       var message = `${data.item_name}(${data.brand_name})`;
       message += ` - ${data.nf_calories}cal / ${data.nf_protein}g (protein) /`
-      message += ` ${data.nf_total_carbohydrates}g (carbs) / ${data.nf_total_fat}g (fat)`;
+      message += ` ${data.nf_total_carbohydrate}g (carbs) / ${data.nf_total_fat}g (fat)`;
 
       res.json({
         "response_type": "in_channel",
