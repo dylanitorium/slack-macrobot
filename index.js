eaa68094882c7f348c29ff35e1b43d6f2a8b9ac7
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-12 23:04:08 +1300

    fix

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ const app = express();
 app.use('/macros', (req, res) => {
   const item = req.query.text.trim();
 
-  var path = 'v1_1/search/';
+  var path = '/v1_1/search/';
   path += encodeURI(item);
   path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat';
   path += '&appId=38256e6d&appKey=66431bb4bbec110e138f0a677cad31f3';
 
   http.get({
-    host: 'api.nutritionix.com/',
+    host: 'api.nutritionix.com',
     path: path,
   }, (getRes) => {
     getRes.on('error', (err) => {
