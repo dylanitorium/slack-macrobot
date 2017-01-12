08b61ceb81e2d9fdee03554a49fb498ca3946499
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-01-12 23:15:37 +1300

    adding protein

--- index.js
+++ index.js
@@ -10,30 +10,12 @@ const options = {
 
 const app = express();
 
-// app.use('/gaf', (req, res) => {
-//   const id = req.query.text.trim();
-//   http.get({
-//     host: 'www.neogaf.com',
-//     path: `/forum/showthread.php?t=${id}`,
-//   }, (getRes) => {
-//     getRes.on('error', (err) => {
-//       console.log(error);
-//     })
-//     getRes.pipe(concat((data) => {
-//       const $ = cheerio.load(data.toString())
-//       res.json({
-//         "response_type": "in_channel",
-//         "text": $('title').text(),
-//       });
-//     }));
-//   });
-// });
 app.use('/macros', (req, res) => {
   const item = req.query.text.trim();
 
   var path = '/v1_1/search/';
   path += encodeURI(item);
-  path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat';
+  path += '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_protein';
   path += '&appId=38256e6d&appKey=66431bb4bbec110e138f0a677cad31f3';
 
   http.get({
@@ -47,7 +29,7 @@ app.use('/macros', (req, res) => {
       var data = JSON.parse(buf.toString()).hits[0].fields;
       res.json({
         "response_type": "in_channel",
-        "text": `${data.item_name}(${data.brand_name}) ${data.nf_calories}(cal) ${data.nf_total_fat}g(total fat)`
+        "text": `${data.item_name}(${data.brand_name}) ${data.nf_calories}(cal) ${data.nf_calories}g (protein) ${data.nf_total_fat}g(total fat)`
       });
     }));
   });
